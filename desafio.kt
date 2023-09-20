1c21f6caef4ef86be7041776decade4994959ec8
Lucas Garcia <60678439+kosakapride@users.noreply.github.com>
2023-09-20 16:19:07 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,55 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel = Nivel.BASICO)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun listarInscritos(): List<Usuario> {
+        return inscritos
+    }
+
+    fun cancelarMatricula(usuario: Usuario) {
+        inscritos.remove(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando alguns objetos para teste
+    val usuario1 = Usuario("Alice")
+    val usuario2 = Usuario("Bob")
+    
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 90, Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", 120, Nivel.INTERMEDIARIO)
+    
+    val formacao = Formacao("Curso de Kotlin", listOf(conteudo1, conteudo2))
+    
+    // Matricular usuários
+    formacao.matricular(usuario1)
+    formacao.matricular(usuario2)
+    
+    // Listar inscritos
+    val inscritos = formacao.listarInscritos()
+    println("Inscritos na formação:")
+    for (inscrito in inscritos) {
+        println(inscrito.nome)
+    }
+    
+    // Cancelar matrícula
+    formacao.cancelarMatricula(usuario1)
+    
+    // Listar inscritos novamente
+    val inscritosAtualizados = formacao.listarInscritos()
+    println("\nInscritos na formação (após cancelamento):")
+    for (inscrito in inscritosAtualizados) {
+        println(inscrito.nome)
+    }
 }
